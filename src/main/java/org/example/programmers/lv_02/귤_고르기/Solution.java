d5c47d48efb6c9b8e673876b4934b015b8253c85
devbloo <zpswl45@naver.com>
2022-12-04 16:18:25 +0900

    12-04 solved refactoring

--- src/main/java/org/example/programmers/lv_02/귤_고르기/Solution.java
+++ src/main/java/org/example/programmers/lv_02/귤_고르기/Solution.java
@@ -1,18 +1,11 @@
 package org.example.programmers.lv_02.귤_고르기;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Solution {
-    // 다른 종류의 수를 최소화하고 싶습니다. 즉, 최대한 비슷하게
-    // tangerine - k 만큼 빼야함
-    // 최소로 등장한 귤을 찾고  tangerine - k 만큼 빼야함
-    // tangerine - k는 등장 횟수가 작은 것부터 누적합이 tangerine - k 되면 됨
-    // k는 한 상자에 몇개 담을 것인지?
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         Map<Integer, Integer> map = new TreeMap<>(); // 귤 size,  개수 cnt
@@ -22,13 +15,7 @@ public class Solution {
 
 
         List<Integer> keyList = new ArrayList<>(map.keySet());
-        Collections.sort(keyList, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return map.get(o2) - map.get(o1);
-            }
-        });
-
+        keyList.sort((o1, o2) -> map.get(o2) - map.get(o1));
 
         for(Integer key : keyList) {
             if (k <= 0)
