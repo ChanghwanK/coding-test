f589f105ff29c03be310686a0bc2c4c70f5cf5b3
devbloo <zpswl45@naver.com>
2022-12-17 20:07:29 +0900

    12-17: sovled-2

---
+++ src/main/java/org/example/baekjoon/문자열/Main.java
@@ -0,0 +1,34 @@
+package org.example.baekjoon.문자열;
+
+import java.util.Scanner;
+
+public class Main {
+    static String A, B;
+
+    static void input() {
+        Scanner sc = new Scanner(System.in);
+        A = sc.next();
+        B = sc.next();
+    }
+
+    // A와 B의 길이가 같으면서, A와 B의 차이를 최소가 되도록 했을 때, 그 차이를 출력하시오.
+    // 최소 차이를 만드는 것이기 때문에 결국에는 같은 문자를 넣어주면 되는 것임
+    // 따라서 추가 연산은 고민할거리가 아니라 우리는 최소 차이를 찾아내면 됨
+    void solve() {
+        int ans = A.length();
+
+        for(int i = 0; i < B.length() - A.length() ; i++) {
+            int cnt = 0;
+            for(int j = 0; j < A.length() ; j++) {
+                if (A.charAt(j) != B.charAt(i + j)) cnt++;
+            }
+            ans = Math.min(ans, cnt);
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+    }
+}
--- src/main/java/org/example/baekjoon/일곱난쟁이/Main.java
+++ src/main/java/org/example/baekjoon/일곱난쟁이/Main.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 
 public class Main {
     static int [] arr = new int[9];
-    static boolean [] used = new boolean[9];
+    static int sum;
 
     static void input(){
         Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 9; i++) arr[i]= sc.nextInt();
 
-        for(int i = 0; i < 9; i++) {
-            arr[i]= sc.nextInt();
-        }
+        sum = Arrays.stream(arr).sum();
     }
 
     // 내가 사고하지 못한 것
@@ -20,9 +19,20 @@ public class Main {
     // 일반적으로 생각해보면 N개중 R개를 뽑아야할 때 R이 너무 크다면 그 반대를 생각해보자
     // 반대의 경우를 찾아 참을 증명하는 것으로 할 수 있음
     static void solve() {
-        Arrays.sort(arr);
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    Arrays.sort(arr);
 
-
+                    for(int k = 2; k < 9; k++) {
+                        System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+        }
 
     }
 
